39b60dcaf5af112ec32b0aa6b721fa21d6f6da56
jaejung Kim <muca0127@gmail.com>
2021-05-13 02:27:08 +0900

    로그 추가

--- src/main/java/com/project/controller/MainController.java
+++ src/main/java/com/project/controller/MainController.java
@@ -3,12 +3,20 @@ package com.project.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import lombok.extern.log4j.Log4j2;
+
+@Log4j2
 @Controller
 public class MainController {
 
     @RequestMapping("/")
     public String viewMain() {
-
+        if (log.isDebugEnabled()) {
+            log.debug("Start viewMain");
+        }
+        if (log.isDebugEnabled()) {
+            log.debug("End viewMain");
+        }
         return "mainPage";
     }
 
